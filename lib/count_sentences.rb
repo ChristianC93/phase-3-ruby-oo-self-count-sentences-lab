5115cca02cd3a015f35a546029be3210c533000d
Christian Castillo <castillochristian3@gmail.com>
2022-09-13 09:29:09 -0400

    commit tree 4655e485b9807d8dcdcc016e576058f2c450d940

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")  
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\!|\?/).filter{|string| string != ""}.count
   end
 end
+
+complex_string = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+complex_string.count_sentences
+
+binding.pry
